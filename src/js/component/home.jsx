eb00679132ea9c1addd967125c30787e5de6f146
iTz-samuel11 <samuelandresfaraco@gmail.com>
2022-05-25 19:32:03 +0000

    inicio de idea

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,40 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
 const Home = () => {
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<React.Fragment>
+			<Counter />
+		</React.Fragment>
 	);
 };
 
 export default Home;
+
+const Counter = () => {
+	return (
+		<div className="d-flex bg-dark">
+			<div>
+				<p className="text-light">{"0"}</p>
+			</div>
+			<div>
+				<p className="text-light">{"0"}</p>
+			</div>
+			<div>
+				<p className="text-light">{"0"}</p>
+			</div>
+			<div>
+				<p className="text-light">{"0"}</p>
+			</div>
+			<div>
+				<p className="text-light">{"0"}</p>
+			</div>
+			<div>
+				<p className="text-light">{"0"}</p>
+			</div>
+			<div>
+				<p className="text-light">{"0"}</p>
+			</div>
+		</div>
+	);
+};
